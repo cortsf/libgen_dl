c78a7b5d4f7d03e00f9def7fcaa5f193a1d72b50
Franco Cortesi <cortesifranco3@gmail.com>
2024-02-06 08:45:15 -0300

    Retry failed downloads

--- libgen_dl.sh
+++ libgen_dl.sh
@@ -2,17 +2,22 @@
 
 echo -e "\n\n###########################################################"
 echo -e "#### Dependencies: pup, aria2c"
-echo -e "#### Usage: 'mkdir <this> && cd <this> && libgen_dl.sh <keywords>' where the <keywords> string contains more than 2 characters."
+echo -e "#### Usage: 'mkdir <blah> && cd <blah> && libgen_dl.sh <keywords>' with length of <keywords> > 2 characters."
 echo -e "#### See ./libgen_dl/log after running. Manually inspect temp files under ./libgen_dl if needed."
 echo -e "#### Optionally comment last line to use uget or any other dl manager to download link list (./libgen_dl/file_link_list.txt)"
+echo -e "#### Alternativelly, use 'libgen_dl --retry' to download links stored in 'libgen_dl/file_download_failures'"
 echo -e "###########################################################\n\n"
 
 [[ "$#" != 1 || "${#1}" -lt 3 ]] && { echo "Invalid arguments. See 'Usage'."; exit 1; }
 
-results=100 # Number of items per search page (Use either 25, 50 or 100).
+[[ "$1" == "--retry" ]] && { 
+    echo "Retrying $(cat libgen_dl/file_download_failures | grep "https" | wc -l) failed downloads";
+    aria2c -j3 -i ./libgen_dl/file_download_failures -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures";
+    exit 1; 
+}
 
 mkSearchLink () {
-    echo "https://libgen.rs/search.php?res=$results&req=$(echo "$1" | sed 's/\ /%20/g')&phrase=0&view=simple&column=def&sort=def&sortmode=ASC&page=$2"
+    echo "https://libgen.rs/search.php?res=100&req=$(echo "$1" | sed 's/\ /%20/g')&phrase=0&view=simple&column=def&sort=def&sortmode=ASC&page=$2"
     }
 
 ######### 0. Create directories, log stuff
@@ -27,7 +32,7 @@ echo "$(date +"%Y-%m-%d %T" ) First page link: $(mkSearchLink "$1" 1)" >> "libge
 aria2c -d "libgen_dl/pages" "$(mkSearchLink "$1" 1)" 
 page_count="$(cat "libgen_dl/pages/search.php" | pup 'script:nth-of-type(2)' | sed '4q;d' | sed 's/,.*//' | sed 's/\ *//')"
 page_count=${page_count:-1}
-echo "$(date +"%Y-%m-%d %T" ) Number of pages ($results items per page): $page_count" >> "libgen_dl/log"
+echo "$(date +"%Y-%m-%d %T" ) Number of pages (100 items per page): $page_count" >> "libgen_dl/log"
 for i in $(seq 2 "$page_count"); do
     aria2c -d "./libgen_dl/pages" "$(mkSearchLink "$1" "$i")"
 done
@@ -52,5 +57,14 @@ done
 
 ######## 5. Download files. Comment this line if you only want to collect links (For example to use another dl manager such as uget)
 aria2c -j3 -i ./libgen_dl/file_link_list.txt -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures"
+fails="$(cat libgen_dl/file_download_failures | grep "https" | wc -l)"  
+[[ $fails -gt 0 ]] && { 
+    msg="$fails failed downloads. Check 'libgen_dl/file_download_failures' and/or retry with 'libgen_dl --retry'."
+    echo -e "\033[31mERROR: $msg"
+    echo "$(date +"%Y-%m-%d %T" ) $msg" >> "libgen_dl/log";
+    exit 1; 
+}
 
-echo "$(date +"%Y-%m-%d %T" ) ======================================== End" >> "libgen_dl/log"
+######## 6. Bye.
+echo "$(date +"%Y-%m-%d %T" ) ======================================== END" >> "libgen_dl/log"
+
