977bffe57ae1da51c1e786650c2a33e3d784d3d8
Franco Cortesi <cortesifranco3@gmail.com>
2024-02-07 20:57:35 -0300

    Decrement --console-log-level, increment --max-tries, store bibtex, use multiple CDNs

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ cd hello
 libgen_dl.sh "hello world"
 ```
 
-libgen_dl.sh downloads files by calling aria2c with `--max-tries 10`, it also collects all failed links (after 10 tries) in `./libgen_dl/file_download_failures`. You can use `libgen_dl.sh --retry` to re-try downloading these failed items. After finishing downloading with `--retry`, libgen_dl will update `./libgen_dl/file_download_failures` and report the number of remaining failed downloads. You can re-run with `--retry` multiple times if necessary. This is to avoid using `--max-tries 0` which could cause ligen_dl to run indefinitely.
+libgen_dl.sh downloads files by calling aria2c with `--max-tries 100`, it also collects all failed links (after 100 tries) in `./libgen_dl/file_download_failures`. You can use `libgen_dl.sh --retry` to re-try downloading these failed items. After finishing downloading with `--retry`, libgen_dl will update `./libgen_dl/file_download_failures` and report the number of remaining failed downloads. You can re-run with `--retry` multiple times if necessary. This is to avoid using `--max-tries 0` which could cause ligen_dl to run indefinitely.
 
 ## Alternatives
 - libgen-cli
--- libgen_dl.sh
+++ libgen_dl.sh
@@ -13,7 +13,7 @@ echo -e "###########################################################\n\n"
 [[ "$1" == "--retry" ]] && { 
     echo "Retrying $(grep -c "https" libgen_dl/file_download_failures) failed downloads";
     echo "$(date +"%Y-%m-%d %T" ) Retrying $(grep -c "https" libgen_dl/file_download_failures) failed downloads"  >> "libgen_dl/log";
-    aria2c -j3 --max-tries 10 --retry-wait 5 -i ./libgen_dl/file_download_failures -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures";
+    aria2c  --console-log-level warn -j3 --max-tries 100 --retry-wait 5 -i ./libgen_dl/file_download_failures -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures" --save-session-interval 2;
     echo "Remaining failed downloads: $(grep -c "https" libgen_dl/file_download_failures)";
     echo "$(date +"%Y-%m-%d %T" ) Remaining failed downloads: $(grep -c "https" libgen_dl/file_download_failures)"  >> "libgen_dl/log";
     exit 0; 
@@ -32,12 +32,12 @@ echo "$(date +"%Y-%m-%d %T" ) Search keywords: $1" >> "libgen_dl/log"
 echo "$(date +"%Y-%m-%d %T" ) First page link: $(mkSearchLink "$1" 1)" >> "libgen_dl/log"
 
 ######## 1. Download all "search" pages.
-aria2c -d "libgen_dl/pages" "$(mkSearchLink "$1" 1)" 
+aria2c --console-log-level warn -d "libgen_dl/pages" "$(mkSearchLink "$1" 1)" 
 page_count="$(grep -m 1 "[0-9]*, // общее число страниц" libgen_dl/pages/search.php | grep -o "[0-9]*")"
 page_count=${page_count:-1}
 echo "$(date +"%Y-%m-%d %T" ) Number of pages (100 items per page): $page_count" >> "libgen_dl/log"
 for i in $(seq 2 "$page_count"); do
-    aria2c -d "./libgen_dl/pages" "$(mkSearchLink "$1" "$i")"
+    aria2c --console-log-level warn -d "./libgen_dl/pages" "$(mkSearchLink "$1" "$i")"
 done
 
 ######## 2. For each "search" page, collect every individual "get" link into `libgen_dl/get_page_link_list.txt`.
@@ -47,23 +47,24 @@ for page_file in ./libgen_dl/pages/*; do
 done
 echo "$(date +"%Y-%m-%d %T" ) Total number of detected items: $(cat libgen_dl/get_page_link_list.txt | wc -l)" >> "libgen_dl/log"
 
-######## 3. Download every "get" page and store it under `libgen_dl/get/`
-aria2c -j3 -d "libgen_dl/get" -i ./libgen_dl/get_page_link_list.txt -l ./libgen_dl/get_page_log
+######## 3. Download every "get" page and store it under `libgen_dl/get/`. Collect metadata.
+aria2c --console-log-level warn -j3 -d "libgen_dl/get" -i ./libgen_dl/get_page_link_list.txt -l ./libgen_dl/get_page_log
+for get_file in ./libgen_dl/get/*; do 
+    echo -e "$(grep '\@book{book:(.*\n)*.*}}' -Pzo $get_file | sed 's///')" >> "libgen_dl/metadata.bib"
+done
 
-
-######## 4. For each individual "get" page collect the direct (document) link into `libgen_dl/file_link_list.txt`.
+######## 4. For each individual "get" page collect the direct (document) link/s into `libgen_dl/file_link_list.txt`.
 for file in ./libgen_dl/get/*; do 
-    if [ -f "$file" ]; then 
-	echo "$(grep -o "https://cdn3.booksdl.org\\\get.php?md5=[a-z0-9]*&key=[A-Z0-9]*" $file | sed 's/\\/\//')" >> "libgen_dl/file_link_list.txt"
-    fi 
+    pseudolink="$(grep -o "booksdl.org\\\get.php?md5=[a-z0-9]*&key=[A-Z0-9]*" $file | sed 's/\\/\//')"
+    echo "https://cdn3.$pseudolink	https://cdn2.$pseudolink" >> "libgen_dl/file_link_list.txt"
 done
 
 ######## 5. Download files. Comment this block if you only want to collect links (For example to use another dl manager such as uget)
-aria2c -j3 --max-tries 10 --retry-wait 5 -i ./libgen_dl/file_link_list.txt -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures"
-fails="$(grep -c "https" libgen_dl/file_download_failures)"  
+aria2c --console-log-level warn -j3 --max-tries 100 --retry-wait 5 -i ./libgen_dl/file_link_list.txt -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures" --save-session-interval 2
+fails="$(grep -c "https" ./libgen_dl/file_download_failures)"
 [[ $fails -gt 0 ]] && { 
-    msg="$fails failed downloads. Check 'libgen_dl/file_download_failures' and/or retry with 'libgen_dl --retry'."
-    echo -e "\033[31mERROR: $msg"
+    msg="$fails failed downloads. Check 'libgen_dl/file_download_failures' and/or retry with 'libgen_dl --retry'.";
+    echo -e "\033[31mERROR: $msg";
     echo "$(date +"%Y-%m-%d %T" ) $msg" >> "libgen_dl/log";
     exit 1; 
 }
