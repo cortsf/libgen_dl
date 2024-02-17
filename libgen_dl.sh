15716074f4727afe5687e09dc11bafd3ae873745
Franco Cortesi <cortesifranco3@gmail.com>
2024-02-17 17:09:22 -0300

    Use libgen.lol besides libgen.li, temporarly disable libgen.li, document current issues with both mirrors in readme.

--- libgen_dl.sh
+++ libgen_dl.sh
@@ -12,20 +12,20 @@ case "$1" in
 	exit 0
 	;;
     "--log")
-	cat libgen_dl/log && exit 0 || exit 1
+	cat libgen_dl/libgen_dl.log && exit 0 || exit 1
 	;;
     "--first")
-	grep -o 'https://.*' ./libgen_dl/log && exit 0 || exit 1
+	grep -o 'https://.*' ./libgen_dl/libgen_dl.log && exit 0 || exit 1
 	;;
     "--retry")
 	[ -f ./libgen_dl/file_download_failures ]  || { echo "Can't find 'libgen_dl/file_download_failures'"; exit 1; }
 	echo "Retrying $(grep -c "https" libgen_dl/file_download_failures) failed downloads"
-	echo "$(date +"%Y-%m-%d %T" ) Retrying $(grep -c "https" libgen_dl/file_download_failures) failed downloads"  >> "libgen_dl/log"
-	aria2c --console-log-level warn -j3 --max-tries 20 --retry-wait 5 -i ./libgen_dl/file_download_failures --log-level notice -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures" --save-session-interval 2 --allow-overwrite true --remove-control-file --content-disposition-default-utf8
+	echo "$(date +"%Y-%m-%d %T" ) Retrying $(grep -c "https" libgen_dl/file_download_failures) failed downloads"  >> "libgen_dl/libgen_dl.log"
+	aria2c --console-log-level warn -j3 --max-tries 20 --retry-wait 5 -i ./libgen_dl/file_download_failures --log-level notice -l ./libgen_dl/file_download.log --save-session "libgen_dl/file_download_failures" --save-session-interval 2 --allow-overwrite true --remove-control-file --content-disposition-default-utf8
 	rem="$(grep -c "https" libgen_dl/file_download_failures)"
 	echo "Remaining failed downloads: $rem"
-	echo "$(date +"%Y-%m-%d %T" ) Remaining failed downloads: $rem"  >> "libgen_dl/log"
-	[[ "$rem" == "0" ]] && echo "$(date +"%Y-%m-%d %T" ) ======================================== END" >> "libgen_dl/log"
+	echo "$(date +"%Y-%m-%d %T" ) Remaining failed downloads: $rem"  >> "libgen_dl/libgen_dl.log"
+	[[ "$rem" == "0" ]] && echo "$(date +"%Y-%m-%d %T" ) ======================================== END" >> "libgen_dl/libgen_dl.log"
 	exit 0 
 	;;
 esac
@@ -36,49 +36,62 @@ mkSearchLink () {
 
 ######### 0. Create directories, log stuff
 mkdir "libgen_dl" || { echo -e "\nRemove the existing './libgen_dl' directory before using this program in a preferably empty directory."; exit; }
-mkdir -p "libgen_dl/get"
-mkdir -p "libgen_dl/pages"
-echo "$(date +"%Y-%m-%d %T" ) ======================================== START " >> "libgen_dl/log"
-echo "$(date +"%Y-%m-%d %T" ) Search keywords: $1" >> "libgen_dl/log"
-echo "$(date +"%Y-%m-%d %T" ) First search page link: $(mkSearchLink "$1" 1)" >> "libgen_dl/log"
+mkdir -p "libgen_dl/get_li"
+mkdir -p "libgen_dl/get_lol"
+mkdir -p "libgen_dl/search_pages"
+mkdir -p "libgen_dl/link_lists"
+echo "$(date +"%Y-%m-%d %T" ) ======================================== START " >> "libgen_dl/libgen_dl.log"
+echo "$(date +"%Y-%m-%d %T" ) Search keywords: $1" >> "libgen_dl/libgen_dl.log"
+echo "$(date +"%Y-%m-%d %T" ) First search page link: $(mkSearchLink "$1" 1)" >> "libgen_dl/libgen_dl.log"
 
 ######## 1. Download all "search" pages.
-aria2c --console-log-level warn -d "libgen_dl/pages" "$(mkSearchLink "$1" 1)" 
-page_count="$(grep -m 1 "[0-9]*, // общее число страниц" libgen_dl/pages/search.php | grep -o "[0-9]*")"
+aria2c --console-log-level warn -d "libgen_dl/search_pages" "$(mkSearchLink "$1" 1)" 
+page_count="$(grep -m 1 "[0-9]*, // общее число страниц" libgen_dl/search_pages/search.php | grep -o "[0-9]*")"
 page_count=${page_count:-1}
-echo "$(date +"%Y-%m-%d %T" ) Number of search pages (100 items per page): $page_count" >> "libgen_dl/log"
+echo "$(date +"%Y-%m-%d %T" ) Number of search pages (100 items per page): $page_count" >> "libgen_dl/libgen_dl.log"
 for i in $(seq 2 "$page_count"); do
-    aria2c --console-log-level warn -d "./libgen_dl/pages" "$(mkSearchLink "$1" "$i")"
+    aria2c --console-log-level warn -d "./libgen_dl/search_pages" "$(mkSearchLink "$1" "$i")"
 done
 
 ######## 2. For each "search" page, collect every individual "get" link into `libgen_dl/get_page_link_list.txt`.
-for page_file in ./libgen_dl/pages/*; do 
-    link="$(grep -o "http://libgen.li/ads.php?md5=[A-Z0-9]*" $page_file)"
-    echo "$link" >> "libgen_dl/get_page_link_list.txt"
+for search_page in ./libgen_dl/search_pages/*; do 
+    echo "$(grep -o "http://library.lol/main/[A-Z0-9]*" $search_page)" >> "libgen_dl/get_page_link_list_lol.txt"
+    echo "$(cat libgen_dl/get_page_link_list_lol.txt | sed 's/http:\/\/library.lol\/main\///')" >> "libgen_dl/md5_list.txt"
+    echo "$(cat libgen_dl/md5_list.txt | sed 's/^/http:\/\/libgen.li\/ads.php\?md5=/')" >> "libgen_dl/get_page_link_list_li.txt"
 done
-echo "$(date +"%Y-%m-%d %T" ) Total number of crawled 'get' page links: $(cat libgen_dl/get_page_link_list.txt | wc -l)" >> "libgen_dl/log"
+echo "$(date +"%Y-%m-%d %T" ) Total number of items: $(cat libgen_dl/md5_list.txt | wc -l)" >> "libgen_dl/libgen_dl.log"
 
-######## 3. Download every "get" page and store it under `libgen_dl/get/`. 
-aria2c --console-log-level warn -j3 -d "libgen_dl/get" -i ./libgen_dl/get_page_link_list.txt -l ./libgen_dl/get_page_log
+# ######## 3. Download every "get" page. 
+aria2c --console-log-level warn -j3 --max-tries 3 --retry-wait 3 -d "libgen_dl/get_lol" -i ./libgen_dl/get_page_link_list_lol.txt -l ./libgen_dl/get_page_lol.log
+# aria2c --console-log-level warn -j3 --max-tries 3 --retry-wait 0 -d "libgen_dl/get_li" -i ./libgen_dl/get_page_link_list_li.txt -l ./libgen_dl/get_page_li.log
 
-######## 4. For each individual "get" page collect the direct (document) link/s into `libgen_dl/file_link_list.txt`. Collect metadata.
-for file in ./libgen_dl/get/*; do 
-    pseudolink="$(grep -o "booksdl.org\\\get.php?md5=[a-z0-9]*&key=[A-Z0-9]*" $file | sed 's/\\/\//')"
-    [[ "$pseudolink" == "" ]] || echo "https://cdn3.$pseudolink	https://cdn2.$pseudolink" >> "libgen_dl/file_link_list.txt"
-    echo -e "$(grep '\@book{book:(.*\n)*.*}}' -Pzo $file | sed 's/\r//')" >> "libgen_dl/metadata.bib"
+
+########## 4. For each individual "get" page collect the direct (document) link/s into `libgen_dl/file_link_list.txt`. Collect metadata.
+cat ./libgen_dl/md5_list.txt | while read hash; do 
+    hash_downcase="$(echo $hash | tr '[:upper:]' '[:lower:]')"
+    get_page_lol="./libgen_dl/get_lol/$hash"
+    echo "$(cat "$get_page_lol" | grep -o "https://download.library.lol/main/[0-9]*/$hash_downcase/[^\"]*" )" >> "libgen_dl/link_lists/libgen_lol.txt"
+    [[ -z "$(ls -A ./libgen_dl/get_li/)" ]] || {
+	link_li_chunk="$(cat ./libgen_dl/get_li/* | grep -o "$hash_downcase&key=[A-Z0-9]*")"
+	[[ "$link_li_chunk" == "" ]] || {
+	    echo "https://cdn3.booksdl.org/get.php?md5=$link_li_chunk" >> "libgen_dl/link_lists/libgen_li_cdn2.txt";
+	    echo "https://cdn2.booksdl.org/get.php?md5=$link_li_chunk" >> "libgen_dl/link_lists/libgen_li_cdn3.txt";
+	    echo -e "$(grep '\@book{book:(.*\n)*.*}}' -Pzo $file | sed 's/\r//')" >> "libgen_dl/metadata.bib"
+	}
+    }
 done
-echo "$(date +"%Y-%m-%d %T" ) Total number of direct (document) links: $(cat libgen_dl/file_link_list.txt | wc -l)" >> "libgen_dl/log"
+echo "$(paste libgen_dl/link_lists/*)" >> libgen_dl/link_lists/combined.txt
 
 ######## 5. Download files. Comment this block if you only want to collect links (For example to use another dl manager such as uget)
-aria2c --console-log-level warn -j3 --max-tries 20 --retry-wait 5 -i ./libgen_dl/file_link_list.txt --log-level notice -l ./libgen_dl/file_download_log --save-session "libgen_dl/file_download_failures" --save-session-interval 2  --allow-overwrite true --content-disposition-default-utf8
-fails="$(grep -c "https" ./libgen_dl/file_download_failures)"
-[[ $fails -gt 0 ]] && { 
-    msg="$fails failed downloads. Check 'libgen_dl/file_download_failures' and/or retry with 'libgen_dl --retry'.";
-    echo -e "\033[31mERROR: $msg";
-    echo "$(date +"%Y-%m-%d %T" ) $msg" >> "libgen_dl/log";
-    exit 1; 
-}
+# aria2c --console-log-level warn -j3 --max-tries 20 --retry-wait 5 -i ./libgen_dl/file_link_list.txt --log-level notice -l ./libgen_dl/file_download.log --save-session "libgen_dl/file_download_failures" --save-session-interval 2  --allow-overwrite true --content-disposition-default-utf8
+# fails="$(grep -c "https" ./libgen_dl/file_download_failures)"
+# [[ $fails -gt 0 ]] && { 
+#     msg="$fails failed downloads. Check 'libgen_dl/file_download_failures' and/or retry with 'libgen_dl --retry'.";
+#     echo -e "\033[31mERROR: $msg";
+#     echo "$(date +"%Y-%m-%d %T" ) $msg" >> "libgen_dl/libgen_dl.log";
+#     exit 1; 
+# }
 
 ######## 6. Bye.
-echo "$(date +"%Y-%m-%d %T" ) ======================================== END" >> "libgen_dl/log"
+echo "$(date +"%Y-%m-%d %T" ) ======================================== END" >> "libgen_dl/libgen_dl.log"
 
